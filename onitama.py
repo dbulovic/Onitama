03924f08a8196db04ee6f27f254ec91708886b23
David Bulovic <david.bulovic@student.tugraz.at>
2022-08-10 15:45:22 +0200

    random cards and small fixes

--- onitama.py
+++ onitama.py
@@ -8,14 +8,11 @@ from tkinter import messagebox
 
 import cards
 
-# turn
+global all_cards, board, red_cards, blue_cards, extra_card, sign
 sign = 1
-
-global old_board, board, bpwn, rpwn, empty, avb, red_cards, blue_cards, extra_card
-red_cards = ["tiger", "elephant"]
-blue_cards = ["monkey", "crab"]
-extra_card = "dragon"
-board = [[" " for x in range(5)] for y in range(5)]
+red_cards = [" ", " "]
+blue_cards = [" ", " "]
+all_cards = ["tiger", "elephant", "monkey", "crab", "dragon"]
 
 def select_card(x, l1, l2, window):
 	global sign, red_cards, blue_cards
@@ -39,82 +36,70 @@ def field_button(i, j, l1, l2, moves, x, window):
 			buttons[ni][nj].config(command=fieldp)
 	set_pieces()
 
-	if sign == 1 and (board[i][j] == "BK" or board[i][j] == "BP"):
+	if (sign == 1 and (board[i][j] == "BK" or board[i][j] == "BP")) or (sign == -1 and (board[i][j] == "RK" or board[i][j] == "RP")):
 		for move in moves:
 			i_new = i+(sign*move[0])
 			j_new = j+(sign*move[1])
 			if i_new < 0 or j_new < 0 or i_new > 4 or j_new > 4: continue
-			if board[i_new][j_new] == "BK" or board[i_new][j_new] == "BP": continue
-			movep = partial(moveb, i, j, i_new, j_new, l1, l2, x, window)
+			if sign == 1 and (board[i_new][j_new] == "BK" or board[i_new][j_new] == "BP"): continue
+			if sign == -1 and (board[i_new][j_new] == "RK" or board[i_new][j_new] == "RP"): continue
+			movep = partial(move_piece, i, j, i_new, j_new, l1, l2, x, window)
 			buttons[i_new][j_new].config(command=movep, image=avb)
 
-	elif sign == -1 and (board[i][j] == "RK" or board[i][j] == "RP"):
-		for move in moves:
-			i_new = i+(sign*move[0])
-			j_new = j+(sign*move[1])
-			if i_new < 0 or j_new < 0 or i_new > 4 or j_new > 4: continue
-			if board[i_new][j_new] == "RK" or board[i_new][j_new] == "RP": continue
-			movep = partial(mover, i, j, i_new, j_new, l1, l2, x, window)	
-			buttons[i_new][j_new].config(command=movep, image=avb)		
-
-
-def moveb(i, j, new_i, new_j, l1, l2, x, window):
+def move_piece(i, j, new_i, new_j, l1, l2, x, window):
 	global sign, extra_card
 	board[new_i][new_j] = board[i][j]
 	board[i][j] = " "
-	l1.config(state=DISABLED)
-	l2.config(state=ACTIVE)
-	sign *= -1
-	for ni in range (5):
-		for nj in range (5):
-			buttons[ni][nj].config(command=False)
-
-	tmp = blue_cards[x % 2]
-	blue_cards[x % 2] = extra_card
-	extra_card = tmp
-	set_cards()
-	set_pieces()
-
-	if check_win() == 1:
-		window.destroy()
-		box = messagebox.showinfo("Winner", "Blue Player won the match")
-
-def mover(i, j, new_i, new_j, l1, l2, x, window):
-	global sign, extra_card
-	board[new_i][new_j] = board[i][j]
-	board[i][j] = " "
-	l1.config(state=ACTIVE)
-	l2.config(state=DISABLED)
-	sign *= -1
-	for ni in range (5):
-		for nj in range (5):
-			buttons[ni][nj].config(command=False)
+	if l1['state'] == DISABLED: l1.config(state=ACTIVE)
+	elif l1['state'] == ACTIVE: l1.config(state=DISABLED)
+	if l2['state'] == DISABLED: l2.config(state=ACTIVE)
+	elif l2['state'] == ACTIVE: l2.config(state=DISABLED)
 	
-	tmp = red_cards[x % 2]
-	red_cards[x % 2] = extra_card
-	extra_card = tmp
+	for ni in range (5):
+		for nj in range (5):
+			buttons[ni][nj].config(command=False)
+
+	if sign == 1:
+		tmp = blue_cards[x % 2]
+		blue_cards[x % 2] = extra_card
+		extra_card = tmp
+	else: 
+		tmp = red_cards[x % 2]
+		red_cards[x % 2] = extra_card
+		extra_card = tmp
+
 	set_cards()
 	set_pieces()
+	sign *= -1
 
-	if check_win() == 2:
-		window.destroy()
+	win_r = check_win()
+	if win_r == 1:
+		box = messagebox.showinfo("Winner", "Blue Player won the match")
+		window.destroy()		
+		play()
+
+	if win_r == 2:
 		box = messagebox.showinfo("Winner", "Red Player won the match")
-
+		window.destroy()		
+		play()
 
 # Create the GUI of game board
 def set_board(window, l1, l2):
-	global board, buttons, bpwn, bkng, rpwn, rkng, empty, avb, rcard1, rcard2, bcard1, bcard2, ecard
+	global board, buttons, bpwn, bkng, rpwn, rkng, empty, temple, avb, rcard1, rcard2, bcard1, bcard2, ecard
 	global tiger, monkey, dragon, elephant, crab
 
 	# setting up the back-end board:
+	board = [[" " for x in range(5)] for y in range(5)]
 	board[0] = ["RP", "RP", "RK", "RP", "RP"]
 	board[4] = ["BP", "BP", "BK", "BP", "BP"]
 	
+	# setting up the images for the pieces and the cards
 	bpwn = PhotoImage(file = r"imgs\bpawn.png")
 	rpwn = PhotoImage(file = r"imgs\rpawn.png")
 	bkng = PhotoImage(file = r"imgs\bking.png")
 	rkng = PhotoImage(file = r"imgs\rking.png")
 	empty = PhotoImage(file = r"imgs\empty.png")
+	temple = PhotoImage(file = r"imgs\temple.png")
 	avb = PhotoImage(file = r"imgs\available.png")
 	tiger = PhotoImage(file = r"imgs\tiger.png")
 	monkey = PhotoImage(file = r"imgs\monkey.png")
@@ -153,11 +138,30 @@ def set_board(window, l1, l2):
 	ecard = Button(window, image=empty, height=128, width=128)
 	ecard.grid(row=7, column=12, rowspan=2, columnspan=2)
 
+	pick_random_cards()
 	set_cards()
 
 	window.mainloop()
 
+def pick_random_cards():
+	global all_cards, red_cards, blue_cards, extra_card
+
+	indices = []
+	for i in range(len(all_cards)):
+		indices.append(i)
+
+	choices = random.sample(indices, k=5)
+
+	red_cards[0] = all_cards[choices[0]]
+	red_cards[1] = all_cards[choices[1]]
+	blue_cards[0] = all_cards[choices[2]]
+	blue_cards[1] = all_cards[choices[3]]
+	extra_card = all_cards[choices[4]]
+
+
 def set_cards():
+	global red_cards, blue_cards, extra_card
+	
 	exec("rcard1.config(image=%s)" % red_cards[0])
 	exec("rcard2.config(image=%s)" % red_cards[1])
 	exec("bcard1.config(image=%s)" % blue_cards[0])
@@ -177,7 +181,10 @@ def set_pieces():
 				buttons[ni][nj].config(image=bkng)
 			elif board[ni][nj] == "RK": 
 				buttons[ni][nj].config(image=rkng)
+			elif (ni == 0 and nj == 2) or (ni == 4 and nj == 2):
+				buttons[ni][nj].config(image=temple)
 			else: buttons[ni][nj].config(image=empty)
+	
 
 def check_win():
 	global board
@@ -191,7 +198,7 @@ def check_win():
 	return 0
 
 # Initial setup
-def pvp(window):
+def game_window(window):
 	window.destroy()
 	window = Tk()
 	window.title("Onitama")
@@ -209,14 +216,14 @@ def play():
 	menu = Tk()
 	menu.geometry("480x480")
 	menu.title("Onitama")
-	wpl = partial(pvp, menu)
+	gwindow = partial(game_window, menu)
 	
 	head = Button(menu, text = "Onitama: Strategy Game",
 				activeforeground = 'red',
 				activebackground = "yellow", bg = "red",
 				fg = "yellow", width = 500, font = 'summer', bd = 5)
 	
-	B1 = Button(menu, text = "Play", command = wpl, activeforeground = 'red',
+	B1 = Button(menu, text = "Play", command = gwindow, activeforeground = 'red',
 				activebackground = "yellow", bg = "red", fg = "yellow",
 				width = 500, font = 'summer', bd = 5)
 	
